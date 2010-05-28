f7c9b065e0231907edb27a45a9f10ec694e2b0e5
srikanthsombhatla <srikanthsombhatla@016151e7-202e-141d-2e90-f2560e693586>
2010-05-28 12:04:51 +0000

    Back light effect first checkin

---
+++ backlight/backlight.pro
@@ -0,0 +1,15 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-05-28T14:02:53
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = backlight
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+HEADERS  +=
---
+++ backlight/main.cpp
@@ -0,0 +1,48 @@
+// Author : Srikanth Sombhatla
+
+#include <QtGui/QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsEllipseItem>
+#include <QGraphicsBlurEffect>
+#include <QGraphicsProxyWidget>
+#include <QPalette>
+#include <QMessageBox>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QGraphicsView view;
+    QPalette viewPallete(view.palette());
+    viewPallete.setBrush(QPalette::Base,QBrush(QColor(0,0,0)));
+    view.setPalette(viewPallete);
+    QGraphicsScene scene;
+    QMessageBox* msgBox = new QMessageBox(&view);
+    msgBox->setText("Do u see backlight effect?");
+    QGraphicsProxyWidget* proxyDlg = scene.addWidget(msgBox);
+
+    const int offset = 0;
+    QGraphicsEllipseItem backLight;
+    QRect backLightRect(proxyDlg->boundingRect().toRect());
+    backLightRect.setSize(QSize(backLightRect.width()+offset,
+                                backLightRect.height()+offset));
+    backLightRect.moveCenter(msgBox->rect().center());
+
+    backLight.setRect(backLightRect);
+    backLight.setZValue(proxyDlg->zValue()-1);
+    backLight.setBrush(QBrush(QColor(Qt::green)));
+    backLight.setPen(QPen(Qt::NoPen));
+    QGraphicsBlurEffect blurEffect;
+    blurEffect.setBlurRadius(30);
+    backLight.setGraphicsEffect(&blurEffect);
+
+    // Reposition dlg and backlight to screen center
+    proxyDlg->setPos(view.rect().center());
+    backLight.setPos(view.rect().center());
+
+    scene.addItem(&backLight);
+    view.setScene(&scene);
+    view.showMaximized();
+    return a.exec();
+}
