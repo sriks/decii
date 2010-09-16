495e2f23db1d28d0e57d0c7d52105c8875221d73
srikanthsombhatla <srikanthsombhatla@016151e7-202e-141d-2e90-f2560e693586>
2010-09-16 14:25:26 +0000

    cchanges to providing parser handle

--- rsssuite/rssmanager/feedprofile.h
+++ rsssuite/rssmanager/feedprofile.h
@@ -18,9 +18,11 @@ public:
     explicit FeedProfile(FeedSubscription subscription,QObject *parent = 0);
     ~FeedProfile();
     bool isValid(){return !mSubscription.sourceUrl().toString().isEmpty();}
+    RSSParser* parser();
 signals:
-    void updateAvailable(RSSParser* parser, int updatedItems);
+    void updateAvailable(QUrl sourceUrl, int updatedItems);
     void error(QString errorDescription);
+
 public slots:
     FeedSubscription subscription(){return mSubscription;}
 
@@ -34,9 +36,15 @@ private slots:
     void handleTimeOut();
     void replyFinished(QNetworkReply* reply);
     void handleContent(QByteArray content);
+
+    // test slots
+    void handleDestroyed(QObject* obj);
+private:
+    QString feedFileName();
 private:
     FeedSubscription mSubscription;
     QString mLatestElementTitle;
+    QString mFeedFileName;
     QTimer mTimer;
     QNetworkAccessManager* mNetworkManager;
 };
