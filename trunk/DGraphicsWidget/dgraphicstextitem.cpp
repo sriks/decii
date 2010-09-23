d36e21a6921bd004486242170a1e1b6f5d6b86b0
srikanthsombhatla <srikanthsombhatla@016151e7-202e-141d-2e90-f2560e693586>
2010-09-23 19:35:06 +0000

    playing with scene rect

--- trunk/DGraphicsWidget/dgraphicstextitem.cpp
+++ trunk/DGraphicsWidget/dgraphicstextitem.cpp
@@ -12,11 +12,14 @@
 DGraphicsTextItem::DGraphicsTextItem(QString text, QGraphicsItem* parent)
                   :QGraphicsWidget(parent)
 {
+    QString fmt("<html><head><body>%1</body></head></html>");
+
     mTextItem = new QGraphicsTextItem(text,this);
+    mTextItem->setHtml(fmt.arg(text)); //test
 }
 
 void DGraphicsTextItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    painter->setBrush(QBrush(QColor(Qt::red)));
-    painter->drawRect(mTextItem->boundingRect().toRect());
+//    painter->setBrush(QBrush(QColor(Qt::red)));
+//    painter->drawRect(mTextItem->boundingRect().toRect());
 }
--- trunk/DGraphicsWidget/main.cpp
+++ trunk/DGraphicsWidget/main.cpp
@@ -1,18 +1,31 @@
 #include <QtGui/QApplication>
 #include <QGraphicsView>
 #include <QGraphicsScene>
+#include <QGraphicsWidget>
+
+#include <QGraphicsLinearLayout>
 #include "dgraphicswidget.h"
+#include "dgraphicstextitem.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QGraphicsScene scene;
     DGraphicsWidget w;
-    w.resize(200,100);
+    w.resize(400,100);
     w.setTitleText("DWidget Title");
+
+    QGraphicsLinearLayout layout(Qt::Vertical);
+    DGraphicsTextItem textitem("This is a nice text to show !!!!!! And let me check how it works with really big text!");
+    layout.addItem(&textitem);
+    textitem.setMaximumWidth(w.size().width());
+    w.addContentLayout(&layout);
+
     scene.addItem(&w);
     QGraphicsView view;
+    //view.setMaximumSize(w.size().toSize());
     view.setScene(&scene);
+    scene.setSceneRect(w.rect());
     view.show();
     return a.exec();
 }
