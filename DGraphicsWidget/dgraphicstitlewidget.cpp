2b00331b7cf39f4c98f0dbb7dab3d815834755a9
srikanthsombhatla <srikanthsombhatla@016151e7-202e-141d-2e90-f2560e693586>
2010-09-29 19:00:06 +0000

    added titlewidget, background gradient

---
+++ DGraphicsWidget/dgraphicstitlewidget.cpp
@@ -0,0 +1,41 @@
+#include <QGraphicsLinearLayout>
+#include "dgraphicstitlewidget.h"
+#include "dgraphicspixmapwidget.h"
+#include "dgraphicstextwidget.h"
+
+DGraphicsTitleWidget::DGraphicsTitleWidget(QGraphicsItem *parent)
+                     :QGraphicsWidget(parent)
+{
+        mTitleText = new DGraphicsTextWidget("",this);
+        mTitlePixmap = new DGraphicsPixmapWidget(QPixmap(),this);
+        mLinearLayout = new QGraphicsLinearLayout(Qt::Horizontal,this);
+        addItemsToLayout();
+}
+
+DGraphicsTitleWidget::~DGraphicsTitleWidget()
+{
+}
+
+void DGraphicsTitleWidget::setTitleText(QString titleText)
+{
+    mTitleText->textItem()->setPlainText(titleText);
+}
+
+void DGraphicsTitleWidget::setTitlePixmap(QPixmap titlePixmap,bool autoResize)
+{
+    // do pixmap trimming here
+    if(autoResize)
+    {
+    int trim = (20*rect().height()/100); //  % of height
+    titlePixmap = titlePixmap.scaled(QSize(rect().height()-trim,rect().height()-trim),
+                                     Qt::IgnoreAspectRatio,
+                                     Qt::SmoothTransformation);
+    }
+    mTitlePixmap->pixmapItem()->setPixmap(titlePixmap);
+}
+
+void DGraphicsTitleWidget::addItemsToLayout()
+{
+    mLinearLayout->addItem(mTitlePixmap);
+    mLinearLayout->addItem(mTitleText);
+}
