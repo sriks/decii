9b01952626a6788eb4da3cdad4f922bc38f82cb7
srikanthsombhatla@gmail.com <srikanthsombhatla@gmail.com@016151e7-202e-141d-2e90-f2560e693586>
2010-10-29 05:15:31 +0000

    reverted to graphicsview. Added mousegrab support for embedded widget. Initial code for horizontal rotation of scroll wheel.

--- cholestrol_analyzer/main.cpp
+++ cholestrol_analyzer/main.cpp
@@ -1,45 +1,36 @@
 #include <QtGui/QApplication>
 #include <QGraphicsView>
 #include <QGraphicsScene>
-#include <QGraphicsProxyWidget>
-#include "qtscrollwheel.h"
+#include <QAction>
 #include "widget.h"
 #include "dcadataentrywidget.h"
-
-#define USE_GV
-#define test
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-#ifdef test
+    DCADataEntryWidget* dataEntryWidget = new DCADataEntryWidget;
     QGraphicsView view;
     QGraphicsScene scene;
-    QGraphicsProxyWidget proxy;
-    QtScrollWheel* scrollWheel = new QtScrollWheel;
-    scrollWheel->setSkin("Beryl");
-    scrollWheel->resize(QSize(80,200));
-    scrollWheel->setMinimum(0);
-    scrollWheel->setMaximum(999);
-    scrollWheel->setSingleStep(1);
-    proxy.setWidget(scrollWheel);
-//    proxy.setFocus(Qt::MouseFocusReason);
-//    proxy.setFlag(QGraphicsItem::ItemIsSelectable,true);
-//    proxy.setFlag(QGraphicsItem::ItemIsMovable,true);
-    scene.addItem(&proxy);
+    scene.addItem(dataEntryWidget);
     view.setScene(&scene);
-//    proxy.grabMouse();
+    view.resize(360,640-200); //n8 size
+
+    // softkeys
+    /* Add new softkey "MyOption" */
+    QAction* newOption = new QAction("OK", &a);
+    /* set soft key for Options */
+    newOption->setSoftKeyRole(QAction::PositiveSoftKey);
+
+    /* Add new softkey "MyOption" */
+    QAction* newExit = new QAction("Cancel", &a);
+    /* set soft key for Exit */
+    newExit->setSoftKeyRole(QAction::NegativeSoftKey);
+
+    QList<QAction *> softKeys;
+    softKeys.append(newOption);
+    softKeys.append(newExit);
+    view.addActions(softKeys);
     view.show();
-    scrollWheel->grabMouse();
-    scene.setFocusItem(&proxy);
-#else
-#ifdef USE_GV
-    DCADataEntryWidget* dataEntryWidget = new DCADataEntryWidget;
-    dataEntryWidget->showMaximized();
-#else
-    Widget w;
-    w.show();
-#endif
-#endif
+
     return a.exec();
 }
