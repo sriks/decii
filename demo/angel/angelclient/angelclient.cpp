288d175529c1c4d36ceb434b19f18c8a07d6ff82
srikanthsombhatla@gmail.com <srikanthsombhatla@gmail.com@016151e7-202e-141d-2e90-f2560e693586>
2010-11-28 20:26:34 +0000

    initial commit.

---
+++ demo/angel/angelclient/angelclient.cpp
@@ -0,0 +1,77 @@
+#include <QtNetwork>
+#include <QDebug>
+#include "angelclient.h"
+#include "ui_angelclient.h"
+
+AngelClient::AngelClient(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::AngelClient)
+{
+    ui->setupUi(this);
+    connect(ui->bindButton,SIGNAL(clicked()),this,SLOT(connectToServer()));
+    mClientSocket = new QTcpSocket(this);
+    connect(mClientSocket,SIGNAL(connected()),this,SLOT(handleHostFound()));
+    connect(mClientSocket,SIGNAL(readyRead()),this,SLOT(handleReadyRead()));
+    connect(mClientSocket,SIGNAL(error(QAbstractSocket::SocketError)),this,SLOT(handleError(QAbstractSocket::SocketError)));
+}
+
+AngelClient::~AngelClient()
+{
+    delete ui;
+}
+
+void AngelClient::handleError(QAbstractSocket::SocketError error)
+{
+    qDebug()<<__FUNCTION__;
+    qDebug()<<error;
+    qDebug()<<mClientSocket->errorString();
+    ui->statusLabel->setText(mClientSocket->errorString());
+
+}
+
+void AngelClient::connectToServer()
+{
+    qDebug()<<__FUNCTION__;
+    QHostAddress hostAddress(ui->hostAddressTextEdit->toPlainText());
+    int port = ui->hostPortTextEdit->toPlainText().toInt();
+    blockSize = 0;
+    mClientSocket->abort();
+    mClientSocket->connectToHost(hostAddress,port);
+}
+
+void AngelClient::handleHostFound()
+{
+    ui->statusLabel->setText("Host found");
+    qDebug()<<"connected";
+}
+
+void AngelClient::handleReadyRead()
+{
+    qDebug()<<__FUNCTION__;
+    QDataStream in(mClientSocket);
+    in.setVersion(QDataStream::Qt_4_0);
+    if (blockSize == 0)
+    {
+        if (mClientSocket->bytesAvailable() < (int)sizeof(quint16))
+        {
+            qDebug()<<"Invalid bytes available";
+            return;
+        }
+        in >> blockSize;
+    }
+
+    if (mClientSocket->bytesAvailable() < blockSize)
+    {
+        qDebug()<<"Invalid block size available";
+        return;
+    }
+
+    qDebug()<<blockSize;
+    QString nextFortune;
+    in >> nextFortune;
+    qDebug()<<nextFortune;
+
+    ui->msgLabel->setText(nextFortune);
+}
+
+// eof
