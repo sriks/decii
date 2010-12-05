778985d899016279ab06d4fc5c067eceeb40f076
srikanthsombhatla@gmail.com <srikanthsombhatla@gmail.com@016151e7-202e-141d-2e90-f2560e693586>
2010-12-05 20:09:17 +0000

    added symbian iap support not via bearer mgmt in qtnetwork. added preliminary slider support.

--- trunk/demo/angel/angelclient/angelclient.cpp
+++ trunk/demo/angel/angelclient/angelclient.cpp
@@ -11,6 +11,7 @@ const QString KResponseStatus = "status";
 const QString KResponseText = "text";
 const QString KResponseType = "responsetype";
 const QString KXqReadResponse = "let $root := doc($xmlsource)//response return data($root/%1)";
+const int KOneSecondInMs = 1000+100;
 
 // commands
 const QByteArray KPlay          = "play";
@@ -19,7 +20,50 @@ const QByteArray KNext          = "next";
 const QByteArray KPrev          = "prev";
 const QByteArray KNowPlaying    = "nowplaying";
 const QByteArray KTrackDuration = "trackduration";
+const QByteArray KTrackPosition = "trackposition";
 const QByteArray KConnect       = "connect";
+
+
+#ifdef Q_OS_SYMBIAN
+#include <es_sock.h>
+#include <sys/socket.h>
+#include <net/if.h>
+
+static void setDefaultIapL()
+    {
+    RSocketServ serv;
+    CleanupClosePushL(serv);
+    User::LeaveIfError(serv.Connect());
+
+    RConnection conn;
+    CleanupClosePushL(conn);
+    User::LeaveIfError(conn.Open(serv));
+    User::LeaveIfError(conn.Start());
+
+    _LIT(KIapNameSetting, "IAP\\Name");
+    TBuf8<50> iap8Name;
+
+    User::LeaveIfError(conn.GetDesSetting(TPtrC(KIapNameSetting), iap8Name));
+    iap8Name.ZeroTerminate();
+
+    conn.Stop();
+    CleanupStack::PopAndDestroy(&conn);
+    CleanupStack::PopAndDestroy(&serv);
+
+    struct ifreq ifReq;
+    memset(&ifReq, 0, sizeof(struct ifreq));
+    strcpy( ifReq.ifr_name, (char*)iap8Name.Ptr());
+    User::LeaveIfError(setdefaultif( &ifReq ));
+}
+
+static int setDefaultIap()
+{
+    TRAPD(err, setDefaultIapL());
+    qDebug()<<"Error in setDefaultIap: "<<err;
+    return err;
+}
+#endif
+
 
 AngelClient::AngelClient(QWidget *parent) :
     QWidget(parent),
@@ -29,6 +73,10 @@ AngelClient::AngelClient(QWidget *parent) :
     mCurrentRequest.clear();
     ui->setupUi(this);
     ui->playPauseButton->setText(KPause);
+//    ui->slider->setSingleStep(1);
+//    ui->slider->setMinimum(0);
+//    ui->slider->setMaximum(0);
+
 //    ui->prevButton->setSkin(KSkin);
 //    ui->nextButton->setSkin(KSkin);
 //    ui->playPauseButton->setSkin(KSkin);
@@ -45,13 +93,40 @@ AngelClient::AngelClient(QWidget *parent) :
     connect(ui->playPauseButton,SIGNAL(clicked()),this,SLOT(playPause()));
     connect(ui->nextButton,SIGNAL(clicked()),this,SLOT(next()));
     connect(ui->prevButton,SIGNAL(clicked()),this,SLOT(prev()));
-
+    connect(ui->slider,SIGNAL(valueChanged(int)),this,SLOT(sliderValueChanged(int)));
+    setDefaultIap();
     connectToServer();
 }
 
 AngelClient::~AngelClient()
 {
     delete ui;
+}
+
+void AngelClient::setupNetworkSession()
+{
+//QNetworkConfigurationManager manager;
+//if (manager.capabilities() & QNetworkConfigurationManager::NetworkSessionRequired) {
+//    // Get saved network configuration
+//    QSettings settings(QSettings::UserScope, QLatin1String("Trolltech"));
+//    settings.beginGroup(QLatin1String("QtNetwork"));
+//    const QString id = settings.value(QLatin1String("DefaultNetworkConfiguration")).toString();
+//    settings.endGroup();
+
+//    // If the saved network configuration is not currently discovered use the system default
+//    QNetworkConfiguration config = manager.configurationFromIdentifier(id);
+//    if ((config.state() & QNetworkConfiguration::Discovered) !=
+//        QNetworkConfiguration::Discovered) {
+//        config = manager.defaultConfiguration();
+//    }
+
+//    networkSession = new QNetworkSession(config, this);
+//    connect(networkSession, SIGNAL(opened()), this, SLOT(sessionOpened()));
+
+//    getFortuneButton->setEnabled(false);
+//    statusLabel->setText(tr("Opening network session."));
+//    networkSession->open();
+//}
 }
 
 QString AngelClient::hostAddressToConnect()
@@ -126,12 +201,26 @@ void AngelClient::readServerResponse()
         else if(KNowPlaying == mCurrentRequest)
         {
             ui->title->setText(responseText);
-            sendRequest(KTrackDuration);
+            trackDuration();
         }
 
         else if(KTrackDuration == mCurrentRequest)
         {
+            mTrackDurationInSec = timeInSecs(responseText);
             ui->duration->setText(responseText);
+//            this->startTimer(KOneSecondInMs);
+            mTrackTimer.start(KOneSecondInMs,this);
+            mSyncTimerId = this->startTimer(KOneSecondInMs*4);
+            trackPosition();
+        }
+
+        else if(KTrackPosition == mCurrentRequest)
+        {
+            ui->console->setText(responseText);
+            int secs = timeInSecs(responseText);
+            ui->slider->setRange(0,mTrackDurationInSec);
+            ui->slider->setValue(secs);
+            mTrackElapsedTime.setHMS(0,0,secs,0);
         }
 
         else if(KConnect == mCurrentRequest)
@@ -168,12 +257,15 @@ void AngelClient::playPause()
     {
         sendRequest(KPlay);
         ui->playPauseButton->setText(KPause);
+        mIsPaused = false;
     }
 
     else if(ui->playPauseButton->text() == KPause)
     {
         sendRequest(KPause);
         ui->playPauseButton->setText(KPlay);
+        mIsPaused = true;
+        mTrackTimer.stop();
     }
 
 }
@@ -187,6 +279,89 @@ void AngelClient::prev()
 {
     sendRequest(KPrev);
 }
+
+void AngelClient::trackDuration()
+{
+    sendRequest(KTrackDuration);
+}
+
+void AngelClient::trackPosition()
+{
+    sendRequest(KTrackPosition);
+}
+
+int AngelClient::timeInSecs(QString aTimeInText)
+{
+    qDebug()<<aTimeInText;
+    QString timeInText = aTimeInText.simplified();
+    QStringList timeList = timeInText.split(":");
+    qDebug()<<timeList<<timeList.count();
+    mHasHourPart = false;
+    if(2 >= timeList.count() && aTimeInText.contains(":"))
+    {
+        int hr = 0;
+        int index = 0;
+        if(3 == timeList.count()) // has hour part also
+        {
+            mHasHourPart = true;
+            hr = timeList.at(index).toInt();
+            ++index;
+        }
+
+    int min = timeList.at(index).toInt();
+    index++;
+    int sec = timeList.at(index).toInt();
+    int result = (hr*60)*60 + min*60 + sec;
+    qDebug()<<hr<<" "<<min<<" "<<sec<<" in sec:"<<result;
+    return result;
+    }
+return -1;
+}
+
+void AngelClient::timerEvent(QTimerEvent *aEvent)
+{
+    if(aEvent->timerId() == mSyncTimerId)
+    {
+        sendRequest(KNowPlaying);
+        ui->console->setText("syncing");
+    }
+    else
+    {
+        if(!mIsPaused)
+        {
+            ui->slider->setValue(ui->slider->value()+1);
+        }
+    }
+}
+
+void AngelClient::sliderValueChanged(int aNewValue)
+{
+    qDebug()<<__FUNCTION__;
+    qDebug()<<aNewValue;
+    if(!mIsPaused && aNewValue <= mTrackDurationInSec)
+    {
+        updateElapsedTime();
+    }
+}
+
+void AngelClient::updateElapsedTime()
+{
+    if(!mIsPaused)
+    {
+        mTrackElapsedTime = mTrackElapsedTime.addSecs(1);
+        QString elapsedTimeText;
+        elapsedTimeText = mTrackElapsedTime.toString("mm") + ":" +
+                          mTrackElapsedTime.toString("ss");
+
+        if(mHasHourPart)
+        {
+            elapsedTimeText.insert(0,mTrackElapsedTime.toString("hh")+":");
+        }
+        ui->elapsed->setText(elapsedTimeText);
+    }
+
+}
+
 
 void AngelClient::sendRequest(QByteArray aRequest)
 {
