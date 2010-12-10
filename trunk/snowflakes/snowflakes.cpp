739fd86b4252b614ef64baaea4dc0a82821a1352
srikanthsombhatla <srikanthsombhatla@016151e7-202e-141d-2e90-f2560e693586>
2010-12-10 14:11:39 +0000

    up and working. first draft.

--- trunk/snowflakes/snowflakes.cpp
+++ trunk/snowflakes/snowflakes.cpp
@@ -2,11 +2,16 @@
 #include <QGraphicsWidget>
 #include <QGraphicsPixmapItem>
 #include <QPropertyAnimation>
+#include <QDesktopWidget>
+#include <QMessageBox>
+#include <QTime>
+#include <QIcon>
 #include <QDebug>
 #include "snowflakes.h"
 
-const int KSnowflakesCreationInterval = 1000*5;
-const int KAnimationDuration          = 1000*5;
+const int KSnowflakesCreationInterval = 50;
+const int KAnimationDuration          = 1000*8;
+const int KOffset                     = 400;
 
 Snowflakes::Snowflakes(QObject* aParent)
 {
@@ -16,14 +21,51 @@ Snowflakes::Snowflakes(QObject* aParent)
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setRenderHints(QPainter::Antialiasing);
     QPalette palette = this->palette();
-    palette.setBrush(QPalette::Base,QBrush(QColor(0,0,0,128))); // transparent bkg
+    palette.setBrush(QPalette::Base,QBrush(QColor(0,0,0,0))); // transparent bkg
     setPalette(palette);
     // Make this view transparent
     setWindowFlags(Qt::FramelessWindowHint);
     setAttribute(Qt::WA_TranslucentBackground,true);
+    setWindowTitle("Snowflakes");
     bool loaded = mSnowflakePixmap.load(":/images/snowflake.png");
     qDebug()<<loaded;
-    show();
+    if(loaded)
+    {
+        setWindowIcon(QIcon(mSnowflakePixmap));
+#ifdef Q_OS_SYMBIAN
+        QSize s(4,4);
+#else
+        QSize s(8,8);
+#endif
+        mSnowflakePixmap = mSnowflakePixmap.scaled(s,
+                                                   Qt::IgnoreAspectRatio,
+                                                   Qt::SmoothTransformation);
+    }
+
+    // resize to full screen
+    QRect mScreenRect = QDesktopWidget().screenGeometry();
+    resize(mScreenRect.size());
+    scene->setSceneRect(mScreenRect);
+    mDirectionOffset = ((QTime().currentTime().msec()) %2 == 0)?(KOffset):(-KOffset);
+    qDebug()<<"diroffset:"<<mDirectionOffset;
+    setFocusPolicy(Qt::StrongFocus);
+
+    // add snowman
+    QPixmap p(":/images/snowman.png");
+    if(!p.isNull())
+    {
+        p = p.scaled(QSize(64,64),
+                     Qt::IgnoreAspectRatio,
+                     Qt::SmoothTransformation);
+    }
+    mSnowMan = new QGraphicsWidget;
+    mSnowMan->setObjectName("snowman");
+    mSnowManPixmapItem = new QGraphicsPixmapItem(p,mSnowMan);
+    scene->addItem(mSnowMan);
+    mSnowManPixmapItem->setToolTip("Click me to close!");
+    mSnowManPixmapItem->setOpacity(0.8);
+    setWindowFlags(Qt::FramelessWindowHint|Qt::WindowStaysOnTopHint);
+    animateSnowMan();
 }
 
 void Snowflakes::startSnowfall()
@@ -36,16 +78,43 @@ void Snowflakes::stopSnowfall()
     killTimer(mTimerId);
 }
 
+void Snowflakes::animateSnowMan()
+{
+    QPropertyAnimation* anim = new QPropertyAnimation(mSnowMan,"pos",this);
+    QPoint snowManPos = rect().bottomRight();
+    snowManPos.setX(snowManPos.x() - mSnowManPixmapItem->pixmap().width());
+    snowManPos.setY( QDesktopWidget().availableGeometry().height() - mSnowManPixmapItem->pixmap().height());
+    QPoint startPoint(rect().width()-mSnowManPixmapItem->pixmap().width(),rect().height()/2);
+    anim->setStartValue(startPoint);
+    anim->setEndValue(snowManPos);
+    anim->setDuration(4000);
+    anim->setEasingCurve(QEasingCurve::OutBounce);
+    anim->start(QAbstractAnimation::DeleteWhenStopped);
+}
+
+void Snowflakes::mousePressEvent(QMouseEvent *aEvent)
+{
+    if(mSnowManPixmapItem == scene()->itemAt(aEvent->posF()))
+    {
+        QMessageBox msg;
+        msg.setText("Close Snowflakes?");
+        msg.setStandardButtons(QMessageBox::Ok|QMessageBox::Cancel);
+        if(QMessageBox::Ok == msg.exec())
+        { close(); }
+    }
+}
+
 void Snowflakes::timerEvent(QTimerEvent *aEvent)
 {
-    QPropertyAnimation* anim = new QPropertyAnimation(this);
+    Q_UNUSED(aEvent);
     QGraphicsWidget* snowflake = createSnowflake();
+    QPropertyAnimation* anim = new QPropertyAnimation(snowflake,"pos",this);
     this->scene()->addItem(snowflake);
-    anim->setTargetObject(snowflake);
-    anim->targetObject()->setParent(anim); //to delete when anim is finished
-    anim->setProperty("pos",QVariant(QPoint()));
-    anim->setStartValue(QPoint(50,50));
-    anim->setEndValue(QPoint(500,500));
+    anim->targetObject()->setParent(anim); //to delete when animation is finished
+    QPoint startPoint(generateX(),-5);
+    QPoint endPoint(startPoint.x()+mDirectionOffset,size().height());
+    anim->setStartValue(startPoint);
+    anim->setEndValue(endPoint);
     anim->setDuration(KAnimationDuration);
     anim->start(QAbstractAnimation::DeleteWhenStopped);
 }
@@ -57,3 +126,13 @@ QGraphicsWidget* Snowflakes::createSnowflake()
     snowflake->setPixmap(mSnowflakePixmap);
     return snowflakeWidget;
 }
+
+int Snowflakes::generateX()
+{
+    static QTime startTime = QTime().currentTime();
+    srand(QTime().currentTime().msecsTo(startTime));
+    int r = rand();
+    return r%size().width();
+}
+
+// eof
