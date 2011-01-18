4daedff3526933e8fc30ec82b6676d5f5094d2e6
srikanthsombhatla <srikanthsombhatla@016151e7-202e-141d-2e90-f2560e693586>
2011-01-18 16:02:08 +0000

    segment view - official working copy

---
+++ KSegmentView/ksegment.h
@@ -0,0 +1,38 @@
+/*
+ *  Author: Srikanth Sombhatla
+ *  Copyright 2010 Konylabs. All rights reserved.
+ *
+ */
+
+#ifndef KSEGMENT_H
+#define KSEGMENT_H
+
+#include <QGraphicsWidget>
+#include <QPointer>
+class QGraphicsLinearLayout;
+//class QPointer;
+class KSegment : public QGraphicsWidget
+{
+    Q_OBJECT
+public:
+    explicit KSegment(Qt::Orientation aOrientation = Qt::Vertical,QGraphicsItem *aParent = 0);
+    QGraphicsLinearLayout* layout() { return mLayout;}
+    void addItem(QGraphicsWidget* aGraphicsLayoutItem);
+    void addItem(QGraphicsItem* aGraphicsItem);
+    void addItem(QWidget* aWidget);
+
+protected slots:
+    void onHeightChanged();
+
+protected:
+    void resizeEvent(QGraphicsSceneResizeEvent *event);
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    float contentHieght() const {return mContentHeight;}
+
+private:
+   QList< QPointer<QGraphicsWidget> > mContentList;
+   QGraphicsLinearLayout* mLayout;
+   float mContentHeight;
+};
+
+#endif // KSEGMENT_H
---
+++ KSegmentView/ksegmentview.cpp
@@ -0,0 +1,107 @@
+/*
+ *  Author: Srikanth Sombhatla
+ *  Copyright 2010 Konylabs. All rights reserved.
+ *
+ */
+
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsLinearLayout>
+#include <QGraphicsWidget>
+#include <QGestureEvent>
+#include <QPanGesture>
+#include <QDebug>
+#include "ksegment.h"
+#include "ksegmentwidget.h"
+#include "KSegmentView.h"
+
+/*!
+    \class KSegmentView
+    \brief A QGraphicsView derived class which encapsulates a container with a layout to hold segments.
+           \ref KSegment, KSegmentWidget or any QGraphicsWidget can be added to this view.
+
+    Internally this view has a container which is a \ref KSegment. Hence a \ref KSegment or \ref KSegmentWidget
+    can be added directly into this view, which inturn arranges the items in the order in which they are added.
+
+    \section usage Usage
+    \code
+        KSegment* labelSegment = new KSegment(Qt::Vertical);
+        labelSegment->layout()->setSpacing(0);
+        labelSegment->addItem(new QGraphicsTextItem("Title"));
+        labelSegment->addItem(new QGraphicsTextItem("Subtitle"));
+
+        KSegment* buttonSegment = new KSegment(Qt::Vertical);
+        buttonSegment->layout()->setSpacing(10);
+        buttonSegment->addItem(new QPushButton("Upload"));
+        QPushButton* removeButton = new QPushButton("Remove");
+        buttonSegment->addItem(removeButton);
+
+        // Now add all segments to a segment widget.
+        //
+        KSegmentWidget* segmentWidget = new KSegmentWidget(Qt::Horizontal);
+        // Segments apppear in the order in which they are added.
+        segmentWidget->addItem(labelSegment);
+        segmentWidget->addItem(buttonSegment);
+
+        KSegmentView segmentView = new KSegmentView(Qt::Vertical);
+        segmentView->addSegmentWidget(segmentWidget);
+    \endcode
+
+    \sa QGraphicsView QGraphicsWidget KSegment KSegmentWidget
+**/
+
+KSegmentView::KSegmentView(Qt::Orientation aOrientation,QWidget *aParent) :
+    QGraphicsView(aParent)
+{
+    QGraphicsScene* s = new QGraphicsScene;
+    setScene(s);
+//    scene()->setSceneRect(0,0,270,10000);
+    mContainer = new KSegment(aOrientation);
+    mContainer->setObjectName("container");
+    mContainer->setPos(0,0);
+    scene()->addItem(mContainer); // ownership is transferred to scene
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+}
+
+/*!
+  Adds a \ref KSegmentWidget to this view. Ownership is transferred.
+  Since \ref KSegmentWidget has a visual appearance, once it is added the size policy is set
+  so that it can grow horizontally, but fixed vertically. This is required for vertical layout
+  since removing an item makes other items to grow vertically.
+
+  \sa addSegment
+**/
+void KSegmentView::addSegmentWidget(KSegmentWidget* aSegmentWidget)
+{
+    aSegmentWidget->setSizePolicy(QSizePolicy::Preferred,QSizePolicy::Preferred);
+    aSegmentWidget->setParentContainer(mContainer->layout());
+    mContainer->addItem(aSegmentWidget);
+}
+
+/*!
+  Adds a \ref KSegment to this view. Ownership is transferred.
+  Use this method to add a \ref KSegmentWidget with a defined sizepolicy.
+  It is always a good practice to define the sizepolicy of items so that they are resized when other
+  items in the layout are removed. By default, a layout tries to resize the items to fit the layout area.
+
+  \sa addSegmentWidget
+**/
+void KSegmentView::addSegment(KSegment* aSegment)
+{
+    mContainer->addItem(aSegment);
+}
+
+void KSegmentView::resizeEvent(QResizeEvent *event)
+{
+    qDebug()<<__PRETTY_FUNCTION__;
+    qDebug()<<"size:"<<size();
+    qDebug()<<"containersize:"<<mContainer->size();
+    qDebug()<<"scene height:"<<scene()->height();
+//    resize(size());
+    if(mContainer->size() != event->size())
+    {
+    mContainer->resize(event->size().width(),mContainer->size().height());
+    }
+}
+
+// eof
