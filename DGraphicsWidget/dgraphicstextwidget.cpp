ca02b3a1a743d280fb21bb4a740f651ca8afe5ea
srikanthsombhatla <srikanthsombhatla@016151e7-202e-141d-2e90-f2560e693586>
2011-01-18 16:07:06 +0000

    resize uses qtextdocument class

--- DGraphicsWidget/dgraphicstextwidget.cpp
+++ DGraphicsWidget/dgraphicstextwidget.cpp
@@ -1,8 +1,10 @@
 #include <QPainter>
 #include <QDebug>
 #include <QWidget>
+#include <QTextDocument>
+#include <QGraphicsSceneResizeEvent>
 #include "dgraphicstextwidget.h"
-
+//#define DEBUG_PAINT
 DGraphicsTextWidget::DGraphicsTextWidget(QString text, QGraphicsItem* parent)
                     :QGraphicsWidget(parent)
 {
@@ -26,5 +28,9 @@ void DGraphicsTextWidget::paint(QPainter *painter, const QStyleOptionGraphicsIte
 void DGraphicsTextWidget::resizeEvent(QGraphicsSceneResizeEvent* event)
 {
     qDebug()<<__PRETTY_FUNCTION__;
+    qDebug()<<"docsize:"<<mTextItem->document()->size();
     mTextItem->setTextWidth(rect().width());
+    QSizeF newSize = mTextItem->document()->size();
+    newSize.setHeight(newSize.height() + ((10*newSize.height())/100) ); // % of height
+    resize(newSize);
 }
